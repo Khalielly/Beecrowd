3c5c70524fe37ed4cb1e224ba9985b3164facf2e
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2025-02-05 09:51:02 -0300

    Resolução do problema: 1009 - Salário com Bônus

---
+++ Iniciante/1009_SalarioComBonus.py
@@ -0,0 +1,5 @@
+nome = str(input())
+salario_fixo = float(input())
+total_vendas = float(input())
+salario_final = salario_fixo + (total_vendas * 0.15)
+print('TOTAL = R$ {:.2f}'.format(salario_final))
